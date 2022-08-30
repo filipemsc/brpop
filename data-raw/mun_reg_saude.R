d361089dc0712fa1ba5d4eea96e8ec158bdcdffe
Raphael Saldanha <rfsaldanha@gmail.com>
2022-08-30 11:24:26 -0300

    New health regions, with 449 units.

--- data-raw/mun_reg_saude.R
+++ data-raw/mun_reg_saude.R
@@ -1,11 +1,9 @@
 ## code to prepare `mun_reg_saude` dataset goes here
 
-mun_reg_saude <- rpcdas::generic_pcdas_query(sql_query = "SELECT res_codigo_adotado AS cod_mun, res_RSAUDCOD AS cod_reg_saude FROM \"datasus-sim\" GROUP BY res_codigo_adotado, res_RSAUDCOD") %>%
+mun_reg_saude <- readr::read_csv2(file = "data-raw/regsaude_449.csv") %>%
   dplyr::mutate(
-    cod_mun = as.character(cod_mun),
-    cod_reg_saude = as.character(cod_reg_saude)
-  ) %>%
-  dplyr::rename(codmun = cod_mun, codregsaude = cod_reg_saude)
-
+    codmun = as.character(codmun),
+    codregsaude = as.character(codregsaude)
+  )
 
 usethis::use_data(mun_reg_saude, overwrite = TRUE)
