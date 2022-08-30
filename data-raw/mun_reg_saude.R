fd8062e1c9d01e20a7e2f5e103feca4053aa21ab
Raphael Saldanha <rfsaldanha@gmail.com>
2022-08-30 10:48:02 -0300

    regsaude changes and new function

--- data-raw/mun_reg_saude.R
+++ data-raw/mun_reg_saude.R
@@ -2,9 +2,10 @@
 
 mun_reg_saude <- rpcdas::generic_pcdas_query(sql_query = "SELECT res_codigo_adotado AS cod_mun, res_RSAUDCOD AS cod_reg_saude FROM \"datasus-sim\" GROUP BY res_codigo_adotado, res_RSAUDCOD") %>%
   dplyr::mutate(
-    cod_mun = as.numeric(cod_mun),
-    cod_reg_saude = as.numeric(cod_reg_saude)
-  )
+    cod_mun = as.character(cod_mun),
+    cod_reg_saude = as.character(cod_reg_saude)
+  ) %>%
+  dplyr::rename(codmun = cod_mun, codregsaude = cod_reg_saude)
 
 
 usethis::use_data(mun_reg_saude, overwrite = TRUE)
